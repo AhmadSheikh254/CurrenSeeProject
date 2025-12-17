4df019de8f998fc70db17e10fa5de32932761642
muhammadssufiyan <sufiyan2308e@aptechsite.net>
2025-12-17 20:52:15 +0500

    .

--- lib/screens/personal_info_screen.dart
+++ lib/screens/personal_info_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
-import 'dart:convert';
 import '../providers/theme_provider.dart';
 import '../providers/auth_provider.dart';
 import '../widgets/animations.dart';
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider/provider.dart';
 import '../providers/theme_provider.dart';
 import '../providers/auth_provider.dart';
 import '../providers/history_provider.dart';
